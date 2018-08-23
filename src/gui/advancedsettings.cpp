b49fa8a769cb0ba6ee25c0bce1e4c620b6737c2f
Rootax <rootsayen@gmail.com>
2018-08-23 19:23:04 +0200

    Add files via upload

--- src/gui/advancedsettings.cpp
+++ src/gui/advancedsettings.cpp
@@ -328,7 +328,7 @@ void AdvancedSettings::loadAdvancedSettings()
     // When build as 32bit binary, set the maximum at less than 2GB to prevent crashes.
     // These macros may not be available on compilers other than MSVC and GCC
 #if defined(__x86_64__) || defined(_M_X64)
-    spinBoxCache.setMaximum(4096);
+    spinBoxCache.setMaximum(12288);
 #else
     // allocate 1536MiB and leave 512MiB to the rest of program data in RAM
     spinBoxCache.setMaximum(1536);
